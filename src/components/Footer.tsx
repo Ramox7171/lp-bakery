f975a26a40d3908132fd73b4a15334e418748602
Mikolaj Lora <loradev@proton.me>
2025-02-05 01:04:21 +0100

    Small fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,10 +25,10 @@ const Footer = () => {
             </ul>
             </div>
             <div>
-            <h3 className="text-md font-semibold mb-4">Platform</h3>
+            <h3 className="text-md font-semibold mb-4">{strings.platformSection}</h3>
             <ul className="space-y-2">
                 {platformLinks.map((link:Link,index:number)=>(
-                    <li key={index}><a className="text-neutral-950 hover:text-white" href={link.href}>{link.text}</a></li>
+                    <li key={index}><a className="text-neutral-950 hover:text-orange-400" href={link.href}>{link.text}</a></li>
                 ))}
             </ul>
             </div>
